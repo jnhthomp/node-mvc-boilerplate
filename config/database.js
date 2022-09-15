6b638df439d59cd8e1780f89ac96e340eb7c83af
Jonah <jnh.thompson@gmail.com>
2022-09-14 17:55:18 -0700

    Add comments through project
    Added excessive comments since this is supposed to be used a
    reference/starting point.
    When creating an application these should be removed/changed as necessary

--- config/database.js
+++ config/database.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // Use mongoose to connect to db
 
+// Database connection method
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
-
+    // Connect to mongo db using connection string from .env
+    const conn = await mongoose.connect(process.env.DB_STRING)
+    // Log db connetion success
     console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
+  } catch (err) { // Handle errors for db connection
+    console.error(err) // log error
+    process.exit(1) // shut down server on error (prevent server hanging)
   }
 }
 
+// export connection method
 module.exports = connectDB
--- routes/todos.js
+++ routes/todos.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const todosController = require('../controllers/todos')
+const express = require('express') // express package for generating router object
+const router = express.Router() // Initialize router object
+const todosController = require('../controllers/todos') // import todos controller containing controller methods 
 
-router.get('/', todosController.getTodos)
-
+// Add routes and associated controller methdods to this router object before exporting (paths relative to 'todo/')
+router.get('/', todosController.getTodos) 
 router.post('/createTodo', todosController.createTodo)
-
 router.put('/markComplete', todosController.markComplete)
-
 router.put('/markIncomplete', todosController.markIncomplete)
-
 router.delete('/deleteTodo', todosController.deleteTodo)
 
+// export created router
 module.exports = router
